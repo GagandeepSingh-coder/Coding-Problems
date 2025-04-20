82cdfca13546fc39e5feac5e6a2d582628b78d1a
Gagandeep Singh <gagandeepsingh9648@gmail.com>
2025-04-20 12:21:34 +0530

    Updated 3SumClosest.java with same logic

--- Arrays/Two-Pointers/3SumClosest.java
+++ Arrays/Two-Pointers/3SumClosest.java
@@ -41,3 +41,31 @@ class Solution {
     }
 }
 // 2 pointers  absolute function used EXTENEDED VERSION OF 3 SUM
+// 2 pointers  absolute function used EXTENEDED VERSION OF 3 SUM
+// approach 2 // same time complexity and space complexity
+class Solution1 {
+    public int threeSumClosest(int[] nums, int target) {
+        int n=nums.length;
+        int closestSum=100000;
+        Arrays.sort(nums);
+        for(int k=0;k<=n-3;k++){
+            int i=k+1;
+            int j=n-1;
+            // 2 pointers
+
+            while(i<j){
+                int sum=nums[k]+nums[i]+nums[j];
+                if(Math.abs(target-sum)<Math.abs(target-closestSum)){
+                    closestSum=sum;
+                }
+                if(sum<target){
+                    i++;
+                }
+                else{
+                    j--;
+                }
+            }
+        }
+        return closestSum;
+    }
+}
